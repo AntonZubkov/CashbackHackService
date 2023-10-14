855f9e350a3870c778c98315af279351a920d633
Anton <An_freiheit@mail.ru>
2023-10-14 16:49:19 +0300

    Initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,30 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void maxLimitValues() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void minLimitedValues() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 999;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1;
+        assertEquals(expected, actual);
+
+    }
+
+}
