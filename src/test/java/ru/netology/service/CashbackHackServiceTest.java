24aedfeeaf2e6d189079ee64e85e83f166f2fe5f
Anton <An_freiheit@mail.ru>
2023-10-14 18:36:05 +0300

    branch testNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,29 +1,30 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.assertEquals;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
-    @Test
-    void maxLimitValues() {
+    @org.testng.annotations.Test
+
+    public void maxLimitValues() {
 
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
         int actual = cashbackHackService.remain(amount);
         int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
 
     }
 
-    @Test
+    @org.testng.annotations.Test
+
     void minLimitedValues() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 999;
         int actual = cashbackHackService.remain(amount);
         int expected = 1;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
 
     }
 
